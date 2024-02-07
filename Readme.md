ed1f343075dccc9b43ab5fa62b7ac4f3d9a8b7ae
Willow Cunningham <willow.e.cunningham@gmail.com>
2024-02-07 17:56:06 -0500

    set up initial build

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Light Wand
+
+
+## Quickstart
+
+    git submodule update --init --recursive
+    mkdir build
+    cd build
+	export PICO_SDK_PATH=/path/to/pico/pico-sdk
+    cmake ..
+    make
+
+## Credits
+
+ - Code, design, and documentation by Willow Cunningham unless otherwise specified
+ - Project advised by Andrew Sheaff
---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#define LED_PIN         25
+
+int main() {
+    stdio_init_all();
+
+    // Delay with some LED blinking on startup
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    for (int i = 0; i < 5; i++) {
+        sleep_ms(500);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+    }
+
+    return 1;
+}
