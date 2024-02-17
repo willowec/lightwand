02012bde06faf7b96f0b3e45846fe41f97625505
William Edward Cunningham <william.e.cunningham@maine.edu>
2024-02-17 16:47:02 -0500

    Figured out 16G range is necessary for ADX

---
+++ docs/notebook.md
@@ -0,0 +1,9 @@
+## 2024/02/17
+
+Testing what acceleration level needs to be selected on the ADXL343 for rapidly waving the stick to not max it out.
+
+- Range = 4G. Gravity = 128. MaxVal = 512
+- Range = 8G. Gravity = 64. MaxVal = 511
+- Range = 16G. Gravity = 32. MaxVal = 275
+
+Because 8G maxxes out, it looks like we will need to use the 16G setting.
--- src/ADXL343.c
+++ src/ADXL343.c
@@ -33,7 +33,7 @@ int adxl343_setup(adxl343 *accelerometer, i2c_inst_t *i2c, uint8_t SDA_pin, uint
         return PICO_ERROR_GENERIC;
 
     // select 16G range, 10-bit measurements: 0b0000 0011 (0x03)
-    err = adxl343_write_register(accelerometer, ADXL3XX_REG_DATA_FORMAT, 0x01);
+    err = adxl343_write_register(accelerometer, ADXL3XX_REG_DATA_FORMAT, 0x03);
     if (err < 0) 
         return err;
 
