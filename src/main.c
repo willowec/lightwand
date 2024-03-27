ef0b36886e4dd427293b3bbfe90f1b8c979c7e4b
William Edward Cunningham <william.e.cunningham@maine.edu>
2024-03-26 22:30:04 -0400

    Added moving average to Jerk

--- src/main.c
+++ src/main.c
@@ -15,9 +15,10 @@
 #define ADX_SDA_PIN     16
 #define ADX_SCL_PIN     17
 
-// defines relating to wand position
+// defines relating to the wand transform
 #define ACCEL_MAX_MSS           30
 #define DIRECTION_TIMEOUT_US    1000 * 500
+#define JERK_AVERAGING_WINDOW_WIDTH     50
 
 // defines relating to text display
 #define COL_SHOW_TIME_US        1000 * 2
@@ -60,17 +61,19 @@ int main() {
     put_15_pixels_on(urgb_u32(0x0f, 0xbf, 0x0f));
     printf("LED's lit green\n");
 
+    // allocate memory for the jerk averaging window
+    double *jerk_history = calloc(sizeof(double), JERK_AVERAGING_WINDOW_WIDTH);
+
     // variables relating to wand position
     int16_t az_raw;
     double accel_mss = 0;
-    double jerk = 0;
+    double average_jerk = 0;
     int dir = 0;    // what direction is the wand moving in
     uint64_t last_moved_time_us = 0;
 
     // variables for tracking changes across frames
     uint64_t prev_now = 0;
     double prev_accel_mss = 0;
-
 
     // variables relating to the text message
     uint64_t last_changed_col_time_us = 0;
@@ -85,22 +88,32 @@ int main() {
         // convert to acceleration in meters/s^2
         accel_mss = (double)az_raw * ADXL3XXVAL_TO_MSS;
 
-        // calculate the jerk of the wand based on the previous frame's acceleration and dt
-        // jerk is NOT calculated to be in m/s^3
-        jerk = (accel_mss - prev_accel_mss) / ((double)(now - prev_now));
+        // calculate the jerk of the wand based on the previous frame's acceleration and dt, and
+        // move it onto the jerk history buffer. jerk is NOT calculated to be in m/s^3
+        for (i = JERK_AVERAGING_WINDOW_WIDTH-1; i > 0; i--) {
+            jerk_history[i] = jerk_history[i-1];
+        }
+        jerk_history[0] = (accel_mss - prev_accel_mss) / ((double)(now - prev_now));;
+
+        // calculate the average jerk
+        average_jerk = 0;
+        for (i = 0; i < JERK_AVERAGING_WINDOW_WIDTH; i++) {
+            average_jerk += jerk_history[i];
+        }
+        average_jerk = average_jerk / JERK_AVERAGING_WINDOW_WIDTH;
 
         // debug print mss
-        printf("%.02f\t%.02f\t%d\n", accel_mss, jerk, dir);
+        printf("%.07f\t%.07f\t%d\n", jerk_history[0], average_jerk, dir);
 
         // update "direction" of stick when the acceleration changes suddenly
-        if (jerk < 0) {
+        if (average_jerk < 0) {
             last_moved_time_us = now;
             dir = -1;
 
             if (message_index == -1) // handle restarting from timeout
                 message_index = MESSAGE_LEN_COLUMNS - 1;
         }
-        else if (jerk > 0) {
+        else if (average_jerk > 0) {
             last_moved_time_us = now;
             dir =  1;
             
@@ -136,6 +149,8 @@ int main() {
         prev_now = now;
        prev_accel_mss = accel_mss;    
     } 
+
+    free(jerk_history);
 
     return 1;
 }
